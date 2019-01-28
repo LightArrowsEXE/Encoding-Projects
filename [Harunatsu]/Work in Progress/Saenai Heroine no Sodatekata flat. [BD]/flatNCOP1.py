564c181c2318566fe07d4a5afac2ab0d68483b74
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-01-28 22:35:10 +0100

    [Harunatsu] Upload SaeKano flat. 01, NCOP, NCED

---
+++ [Harunatsu]/Work in Progress/Saenai Heroine no Sodatekata flat. [BD]/flatNCOP1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = '/home/lightarrowsexe/Desktop/00009.m2ts'
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+vid = ac.split(src, [(24,2183)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('track1_jpn.flac', video_source=ts_in)
+       
