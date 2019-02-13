89ea0dd94661a7ff781c22702215d7dd9dadcfbb
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-02-13 01:50:34 +0100

    [Kaleido] Update PPD OP1, 01, 02; Upload PPD ED1-12, AC files

---
+++ [Kaleido-subs]/Work in Progress/Pani Poni Dash/ac_02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r"ac.mkv"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(36466,70777])])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('02_cut.m4a', audio_source=r'[BDMV][Pani Poni Dash][ぱにぽにだっしゅ！]\VOL1\BDMV\STREAM\00005.m4a')
+    
---
+++ [Kaleido-subs]/Work in Progress/Pani Poni Dash/ac_ED04.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r"ac.mkv"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,2181)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ED4_cut.m4a', audio_source=r'00020_Track01.m4a')
+    
