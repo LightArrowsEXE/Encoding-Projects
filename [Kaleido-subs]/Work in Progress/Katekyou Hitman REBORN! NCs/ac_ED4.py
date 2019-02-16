3fc5831ea47aa3bf4565edb51ca9e0b567c30213
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-02-16 06:51:06 +0100

    [Kaleido] Upload KHR ED1-8

---
+++ [Kaleido-subs]/Work in Progress/Katekyou Hitman REBORN! NCs/ac_ED4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r"[BDMV][Katekyou Hitman REBORN!]\[家庭教師ヒットマンREBORN！][2006][TV][BDMV][Blu-ray BOX 1][JP][20170419]\REBORN_DISC9\BDMV\STREAM\00009.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0,2697)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ED4_cut.m4a', audio_source=r'[BDMV][Katekyou Hitman REBORN!]\[家庭教師ヒットマンREBORN！][2006][TV][BDMV][Blu-ray BOX 1][JP][20170419]\REBORN_DISC9\BDMV\STREAM\00009_Track01.m4a')
+    
