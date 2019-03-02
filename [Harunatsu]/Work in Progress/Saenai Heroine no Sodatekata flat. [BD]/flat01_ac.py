a8c9baea3262b202e71edcfdec89a638b37eaa8b
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-03-02 04:06:20 +0100

    [Harunatsu] Update SaeKano flat.

---
+++ [Harunatsu]/Work in Progress/Saenai Heroine no Sodatekata flat. [BD]/flat01_ac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r'[BDMV]冴えない彼女の育てかた♭Vol.01~Vol.06/[BDMV]冴えない彼女の育てかた♭ VOL.01/BDMV/STREAM/00002.m2ts'
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0,32846)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('flat01_audiocut.m4a', audio_source='[BDMV]冴えない彼女の育てかた♭Vol.01~Vol.06/[BDMV]冴えない彼女の育てかた♭ VOL.01/BDMV/STREAM/00002_Track01.m4a')
