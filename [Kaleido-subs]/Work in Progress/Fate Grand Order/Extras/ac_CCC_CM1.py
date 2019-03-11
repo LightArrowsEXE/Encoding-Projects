d19bd858b1e5aaa7597c6c6013778294733e7521
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-03-11 09:17:49 +0100

    [Kaleido] Upload F/GO×CCC CM

---
+++ [Kaleido-subs]/Work in Progress/Fate Grand Order/Extras/ac_CCC_CM1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+
+core = vs.core
+ts_in = r"../[BDMV][Fate／Grand Order -MOONLIGHT／LOSTROOM-]/BDROM/BDMV/STREAM/00006.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,383)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'CMs/CCC_CM1_cut.flac', audio_source=r'../[BDMV][Fate／Grand Order -MOONLIGHT／LOSTROOM-]/BDROM/BDMV/STREAM/00006.flac')
