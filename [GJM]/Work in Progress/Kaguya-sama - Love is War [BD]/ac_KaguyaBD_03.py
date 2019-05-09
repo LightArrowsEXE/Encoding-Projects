0c7b4067b51044f01a23208c1f88c9210e1c932a
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-05-09 23:38:51 +0200

    [GJM] Upload Kaguya 03 & 04 AC, NCED2 (BD)

---
+++ [GJM]/Work in Progress/Kaguya-sama - Love is War [BD]/ac_KaguyaBD_03.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+
+core = vs.core
+ts_in = r"BDMV/かぐや様は告らせたい Vol.2/BD/BDMV/STREAM/00001.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0,34523)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'KaguyaBD_03_cut.m4a', audio_source=r'BDMV/かぐや様は告らせたい Vol.2/BD/BDMV/STREAM/00001_Track01.m4a')
