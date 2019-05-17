1bdbeda09190a8e687a540fb847f397694a6a8b6
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-05-17 03:08:33 +0200

    [RTW] Upload Owari 2 NCs

---
+++ Miscellaneous groups/[RTW]/Work in Progress/Owarimonogatari (2017)/Owari2BD_NCED1_ac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+
+
+core = vs.core
+ts_in = r"BDMV/[BDMV][アニメ][171025] 「終物語」 第六巻／まよいヘル/BD_VIDEO/BDMV/STREAM/00010.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(12,2171)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'OwariBD_2_NCED1_cut.m4a', audio_source=r'BDMV/[BDMV][アニメ][171025] 「終物語」 第六巻／まよいヘル/BD_VIDEO/BDMV/STREAM/00010.m4a')
