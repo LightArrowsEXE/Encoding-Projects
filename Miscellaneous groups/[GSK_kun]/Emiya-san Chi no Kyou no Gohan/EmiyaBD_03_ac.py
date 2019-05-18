1225a477c77d08600fc6c223a2bfc7d832e98908
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-05-18 02:36:42 +0200

    [GSK] Upload Emiya 01-02 AC

---
+++ Miscellaneous groups/[GSK_kun]/Emiya-san Chi no Kyou no Gohan/EmiyaBD_03_ac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+
+
+core = vs.core
+ts_in = r"BDMV/衛宮さんちの今日のごはん/Vol 1/BDROM/BDMV/STREAM/00003.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,18005)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'03/EmiyaBD_03_cut.m4a', audio_source=r'BDMV/衛宮さんちの今日のごはん/Vol 1/BDROM/BDMV/STREAM/00003.m4a')
