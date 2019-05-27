9c8f5b31bf62976819057b5a5750043534852fc8
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-05-28 00:52:15 +0200

    [Harunatsu] Re-done SaeKano flat (BD)
    * Rewrote encoding scripts
    * Rewrote audio cutter scripts
    * Add fixes frames

---
+++ Miscellaneous groups/[Harunatsu]/Work in Progress/Saenai Heroine no Sodatekata flat. [BD]/ac_SaeKano2BD_04.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r'[BDMV]冴えない彼女の育てかた♭Vol.01~Vol.06/[BDMV]冴えない彼女の育てかた♭ VOL.03/BDMV/STREAM/00001.m2ts'
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,32872)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ac_SaeKano2BD_04.m4a', audio_source='[BDMV]冴えない彼女の育てかた♭Vol.01~Vol.06/[BDMV]冴えない彼女の育てかた♭ VOL.03/BDMV/STREAM/00001.flac')
