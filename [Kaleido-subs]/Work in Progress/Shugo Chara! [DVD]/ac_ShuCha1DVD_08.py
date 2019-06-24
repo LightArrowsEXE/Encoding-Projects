450078fa2f7918ffe4893fb359b95ab41822f502
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-06-24 09:18:18 +0200

    [Kaleido] Upload Shugo Chara (DVD) cour 1

---
+++ [Kaleido-subs]/Work in Progress/Shugo Chara! [DVD]/ac_ShuCha1DVD_08.py
@@ -0,0 +1,25 @@
+    #!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+core = vs.core
+
+
+ts_in = r"DVDISO/[DVDISO][アニメ][SHUGO CHARA!][Vol.1-16 Fin]/SHUGO_CHARA_03.d2v"
+src = lvf.src(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+if ts_in.endswith('.d2v'):
+    src = core.vivtc.VFM(src, 1)
+    src = core.vivtc.VDecimate(src)
+
+vid = ac.split(src, [(36749, 71753)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'ShuCha1DVD_08_cut.m4a', audio_source=r'DVDISO/[DVDISO][アニメ][SHUGO CHARA!][Vol.1-16 Fin]/SHUGO_CHARA_03.m4a')
---
+++ [Kaleido-subs]/Work in Progress/Shugo Chara! [DVD]/ac_ShuCha1DVD_NCOP1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+
+
+core = vs.core
+ts_in = r"DVDISO/[DVDISO][アニメ][SHUGO CHARA!][Vol.1-16 Fin]/SHUGO_CHARA_BOX01_TOKUTEN.d2v"
+src = lvf.src(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(36390,39087)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'ShuCha1DVD_NCOP1_cut.m4a', audio_source=r'DVDISO/[DVDISO][アニメ][SHUGO CHARA!][Vol.1-16 Fin]/SHUGO_CHARA_BOX01_TOKUTEN.d2v Ta0 stereo 1536 kbps.m4a')
