39a613b5790b3ad9e73b021e777669b4a19b72c6
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-06-24 09:13:01 +0200

    [GJM] Upload Kaguya 05-06 ACs (BD)

---
+++ [GJM]/Work in Progress/Kaguya-sama - Love is War [BD]/ac_KaguyaBD_05.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+
+core = vs.core
+ts_in = r"BDMV/[BDMV][190529][[Kaguya-sama - Love Is War][Vol.3]/BDMV/STREAM/00001.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0,34526)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'KaguyaBD_05_cut.m4a', audio_source=r'BDMV/[BDMV][190529][[Kaguya-sama - Love Is War][Vol.3]/BDMV/STREAM/00001.m4a')
