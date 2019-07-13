583d5dfae27b406931a437df1fe87164773ac6a4
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-07-13 22:43:40 +0200

    [SmugCat] Upload Aria 01 & 02 + ACs (BD) + README.md

---
+++ Miscellaneous groups/[SmugCat]/Aria the Animation [BD]/ac_Aria1BD_01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r'BDMV/ARIA SEASON ONE BD/ARIAS1BD1/BDMV/STREAM/00002.m2ts'
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(30,43830)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ac_Aria1BD_02.m4a', audio_source='BDMV/ARIA SEASON ONE BD/ARIAS1BD1/BDMV/STREAM/00002.m4a')
