de2eef7419acc10d57a73acfd7a9b1b9f06f0d35
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-07-23 01:19:39 +0200

    [Kaleido] Upload Shield Hero 08 & AC (BD)

---
+++ [Kaleido-subs]/Work in Progress/Tate no Yuusha no Nariagari [BD]/ac_Shieldhero_08.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+
+
+core = vs.core
+ts_in = r'BDMV/[BDMV][190524][Tate no Yuusha no Nariagari][Vol.2]/TATE_2_1/BDMV/STREAM/00009.m2ts'
+src = lvf.src(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,34070)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+
+if __name__ == "__main__":
+    ac.cut_audio(r'ShieldbroBD_08_cut.m4a', audio_source=r'BDMV/[BDMV][190524][Tate no Yuusha no Nariagari][Vol.2]/TATE_2_1/BDMV/STREAM/00009.m4a')
