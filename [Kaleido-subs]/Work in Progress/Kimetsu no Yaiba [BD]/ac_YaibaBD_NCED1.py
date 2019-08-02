91ee5d06209afe397f88bdf4dbc7a10e11d7828f
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-08-02 15:19:12 +0200

    [Kaleido] Upload Yaiba Vol.1 (BD)
    Includes:
    * Scripts for Eps1-2, NCOP1/ED1
    * Audio Cutter files for Eps1-2, NCOP1/ED1
    * Encoding settings

---
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_NCED1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+
+
+core = vs.core
+ts_in = r'BDMV/[BDMV][190731][Kimetsu no Yaiba][Vol.1]/BDMV/STREAM/00008.m2ts'
+src = lvf.src(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0, 2159)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+
+if __name__ == "__main__":
+    ac.cut_audio(r'YaibaBD_NCED1_cut.m4a', audio_source=r'BDMV/[BDMV][190731][Kimetsu no Yaiba][Vol.1]/BDMV/STREAM/00008.m4a')
