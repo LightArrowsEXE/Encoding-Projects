782ce2eb16d8f1856cafc3532060152e7d0dc064
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-08-22 08:27:59 +0200

    [SmugCat] Upload Aria NCs + settings
    whoops, I somehow missed these initially. Done now.

---
+++ Miscellaneous groups/[SmugCat]/Completed/Aria the Animation [BD]/ac_Aria1BD_ED1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import shutil
+import os
+
+core = vs.core
+ts_in = r'G:/Seed/ARIA SEASON ONE BD/ARIAS1BD3/BDMV/STREAM/00013.m2ts'
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(30,2851)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ac_Aria1BD_ED1.m4a', audio_source='G:/Seed/ARIA SEASON ONE BD/ARIAS1BD3/BDMV/STREAM/00013.m4a')
