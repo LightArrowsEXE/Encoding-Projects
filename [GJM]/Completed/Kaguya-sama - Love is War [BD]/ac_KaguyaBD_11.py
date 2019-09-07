8848e33e59d6ffe882b71aa8deee48bdad438b12
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-09-07 19:34:06 +0200

    [GJM] Upload Kaguya Vol.6 (BD) ACs

---
+++ [GJM]/Completed/Kaguya-sama - Love is War [BD]/ac_KaguyaBD_11.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+
+core = vs.core
+ts_in = r"BDMV/かぐや様は告らせたい Vol.6/BD/BDMV/STREAM/00001.m2ts"
+src = src = lvf.src(ts_in).std.AssumeFPS(fpsnum=24000, fpsden=1001)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(0, 34525)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio(r'KaguyaBD_11_cut.m4a', audio_source=r'BDMV/かぐや様は告らせたい Vol.6/BD/BDMV/STREAM/00001.m4a')
