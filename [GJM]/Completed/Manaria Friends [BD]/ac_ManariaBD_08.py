bf0f3864d320cd5bbb80c829096b25fbfb9bdd4c
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-09-22 01:22:37 +0200

    [GJM] Update Manaria Friends BD + Upload settings

---
+++ [GJM]/Completed/Manaria Friends [BD]/ac_ManariaBD_08.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import os
+
+# Note: before cutting
+preview = 16688
+endcard = 16808
+part_b = 16928
+epend = 20164
+
+# Note: after cutting
+edstart = 14534
+
+
+core = vs.core
+ts_in = r"BDMV/[BDMV][190402][マナリアフレンズ II]/BD/BDMV/STREAM/00009.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(24,preview-1),(endcard,endcard+48),(part_b,epend),(preview,part_b)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ManariaBD_08_ac.m4a', audio_source='BDMV/[BDMV][190402][マナリアフレンズ II]/BD/BDMV/STREAM/00009.m4a')
+
+os.remove("tmp-001.mka")
+os.remove("tmp-002.mka")
+os.remove("tmp-003.mka")
+os.remove("tmp-004.mka")
---
+++ [GJM]/Completed/Manaria Friends [BD]/ac_ManariaBD_NCED2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+from subprocess import call
+import os
+
+core = vs.core
+ts_in = r"BDMV/[BDMV][190302][マナリアフレンズ I]/BD/BDMV/STREAM/00012.m2ts"
+src = core.lsmas.LWLibavSource(ts_in)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(983+48,1964+47)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+if __name__ == "__main__":
+    ac.cut_audio('ManariaBD_NCED2_ac.m4a', audio_source='BDMV/[BDMV][190302][マナリアフレンズ I]/BD/BDMV/STREAM/00012.m4a')
