cd7941389ace72f72352f5606077dc096dc6facb
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-10-13 03:57:54 +0200

    [Kaleido] Upload Babylonia 01-02 (AC)

---
+++ [Kaleido-subs]/Currently Airing/FGO - Absolute Demonic Front - Babylonia/ac_Babylonia_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import vapoursynth as vs
+import audiocutter
+import lvsfunc as lvf
+from subprocess import call
+
+
+core = vs.core
+ts_in = r'01/Fate Grand Order_ Zettai Majuu Sensen Babylonia - 01 (MX).d2v'
+src = lvf.src(ts_in)
+
+if ts_in.endswith('d2v'):
+    src = core.vivtc.VDecimate(src)
+
+ac = audiocutter.AudioCutter()
+
+vid = ac.split(src, [(844,3519),(5198,19343),(20783,37971-31)])
+
+ac.ready_qp_and_chapters(vid)
+
+vid.set_output(0)
+
+if __name__ == "__main__":
+    ac.cut_audio(r'Babylonia_01_cut.m4a', audio_source=r'01/Fate Grand Order_ Zettai Majuu Sensen Babylonia - 01 (MX) T112 stereo 255 kbps DELAY -310 ms.aac')
