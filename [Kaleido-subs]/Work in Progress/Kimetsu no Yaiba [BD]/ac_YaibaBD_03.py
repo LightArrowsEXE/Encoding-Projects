0640c268babdd368e36c29222282edc0b3fa8fdd
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-10-22 03:08:58 +0200

    [Kaleido] Update Yaiba vol.1-2 (BD)

--- [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_03.py
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_03.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
-
-import vapoursynth as vs
-import audiocutter
+import acsuite as acs
 import lvsfunc as lvf
-from subprocess import call
+ac = acs.AC()
 
 
-core = vs.core
-ts_in = r'BDMV/[BDMV][190828][Kimetsu no Yaiba][Vol.2]/BDMV/STREAM/00004.m2ts'
-src = lvf.src(ts_in)
-
-ac = audiocutter.AudioCutter()
-
-vid = ac.split(src, [(0, 34045)])
-
-ac.ready_qp_and_chapters(vid)
-
-vid.set_output(0)
+path = r'BDMV/[BDMV][190828][Kimetsu no Yaiba][Vol.2]/BDMV/STREAM/00004.m2ts'
+src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.cut_audio(r'YaibaBD_03_cut.m4a', audio_source=r'BDMV/[BDMV][190828][Kimetsu no Yaiba][Vol.2]/BDMV/STREAM/00004.m4a')
+    ac.eztrim(src, [(0, -26)], path[:-4]+"wav", "YaibaBD_03_cut.wav")
