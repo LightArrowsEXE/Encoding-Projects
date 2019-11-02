2606f12d200dc351344e9e3c3ae8a98f94f1ce21
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-11-02 16:02:21 +0100

    [Kaleido] Upload Yaiba Vol.4 AC (BD)

---
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_09.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite as acs
+import lvsfunc as lvf
+ac = acs.AC()
+
+
+path = r'BDMV/[BDMV][191030][Kimetsu no Yaiba][Vol.4]/BDMV/STREAM/00005.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(0, -24)], path[:-4]+"wav", "YaibaBD_09_cut.wav")
