22427bde96be945c6d01d2cb2dea18939262760d
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-12-05 09:16:58 +0100

    [GSK_] Upload Machikado Vol.1-2 (BD)

---
+++ Miscellaneous groups/[GSK_kun]/Work in Progress/Machikado Mazoku [BD]/ac_MachikadoBD_06.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite
+import lvsfunc as lvf
+ac = acsuite.AC()
+
+
+path = r'BDMV/[BDMV][191106][Machikado Mazoku][Vol.2]/MACHIKADO_MAZOKU_2/BDMV/STREAM/00007.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(0, -24)], path[:-4]+"wav", "MachikadoBD_06_cut.wav")
