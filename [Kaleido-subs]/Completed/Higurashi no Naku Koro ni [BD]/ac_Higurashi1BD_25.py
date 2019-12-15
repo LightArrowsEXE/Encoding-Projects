c3a9df36adc8436772ae053595b18b9f7add06cc
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-12-15 18:59:46 +0100

    [Kaleido] Upload Higurashi (BD)

---
+++ [Kaleido-subs]/Completed/Higurashi no Naku Koro ni [BD]/ac_Higurashi1BD_25.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import vapoursynth as vs
+import acsuite
+import lvsfunc as lvf
+ac = acsuite.AC()
+core = vs.core
+
+
+path = r'BDMV/HIGURASHI_BD/00026.m2ts'
+src = lvf.src(path)
+src = core.vivtc.VDecimate(src)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(0, -24)], path[:-4]+"wav", "Higurashi1BD_25_cut.wav")
