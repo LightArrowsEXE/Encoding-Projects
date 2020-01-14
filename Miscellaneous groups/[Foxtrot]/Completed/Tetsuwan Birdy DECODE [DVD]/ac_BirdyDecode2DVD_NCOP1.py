143a2833f4a85e4e7e98fc7bb000a05984ccd907
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-01-14 16:06:42 +0100

    [Foxtrot] Move Birdy Decode (DVD) to "Completed"

---
+++ Miscellaneous groups/[Foxtrot]/Completed/Tetsuwan Birdy DECODE [DVD]/ac_BirdyDecode2DVD_NCOP1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite
+import lvsfunc as lvf
+ac = acsuite.AC()
+
+
+path = r'DVDISO/Tetsuwan_Birdy_THE_CIPHER/TETSUWAN_BIRDY02_07.d2v'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(101830, 104528)], "DVDISO/Tetsuwan_Birdy_THE_CIPHER/TETSUWAN_BIRDY02_07 Ta0 stereo 1536 kbps DELAY 0 ms.w64", "BirdyDecode2DVD_NCOP1_cut.wav")
