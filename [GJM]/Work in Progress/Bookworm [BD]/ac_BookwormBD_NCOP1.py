b39ac87ea6e6b282b90215ca80a2794a716b6fa3
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-01-14 15:51:54 +0100

    [GJM] Upload Bookworm Vol.1 2/2, update scripts overall (BD)

---
+++ [GJM]/Work in Progress/Bookworm [BD]/ac_BookwormBD_NCOP1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite as acs
+import lvsfunc as lvf
+ac = acs.AC()
+
+
+path = r'BDMV/[BDMV] HONZUKI/HONZUKI_2/BDMV/STREAM/00011.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(24, -24)], path[:-4]+"wav", "BookwormBD_NCOP1_cut.wav")
