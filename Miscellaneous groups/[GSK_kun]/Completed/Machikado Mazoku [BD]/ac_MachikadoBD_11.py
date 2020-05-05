578322fc136c3103153388a5a172aaa206dfb167
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-05-05 04:43:50 +0200

    [GSK_kun] Upload Macihkado (BD) 10-12 AC

---
+++ Miscellaneous groups/[GSK_kun]/Completed/Machikado Mazoku [BD]/ac_MachikadoBD_11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite
+import lvsfunc as lvf
+ac = acsuite.AC()
+
+
+path = r'BDMV/[BDMV][200108][Machikado Mazoku][Vol.4]/MACHIKADO_MAZOKU_4/BDMV/STREAM/00006.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(0, -24)], path[:-4]+"wav", "MachikadoBD_11_cut.wav")
