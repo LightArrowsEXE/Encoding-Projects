b1f36d8d4bad4ce5346f35314402251d3ef390a4
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-05-24 22:53:49 +0200

    [Kaleido] Upload Joshiraku Vol.1 (BD)

---
+++ [Kaleido-subs]/Work in Progress/Joshiraku [BD]/ac_JoshirakuBD_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import acsuite as acs
+import lvsfunc as lvf
+ac = acs.AC()
+
+
+path = r'BDMV/JOSHIRAKU/120926_JOSHIRAKU_VOL1/BDMV/STREAM/00000.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    ac.eztrim(src, [(34817,-42)], path[:-4]+"wav", "JoshirakuBD_02_cut.wav")
