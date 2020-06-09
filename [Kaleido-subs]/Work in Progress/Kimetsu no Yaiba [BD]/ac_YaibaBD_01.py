0ba9e22bd93f22a8061f42b97be9ebc0e9b1f289
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-10 00:34:47 +0200

    [Kaleido] Update Yaiba (BD) 01-07, NCOP1v1-4/ED1, settings

--- [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_01.py
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_01.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import acsuite as acs
 import lvsfunc as lvf
 ac = acs.AC()
@@ -8,4 +9,4 @@ path = r'BDMV/[BDMV][190731][Kimetsu no Yaiba][Vol.1]/BDMV/STREAM/00004.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, [(0, -27)], path[:-4]+"wav", "YaibaBD_01_cut.wav")
+    ac.eztrim(src, [(0, -27)], f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
--- [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_NCOP1v3.py
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_NCOP1v3.py
@@ -5,7 +5,7 @@ import lvsfunc as lvf
 ac = acs.AC()
 
 
-path = r'BDMV/[BDMV][190731][Kimetsu no Yaiba][Vol.1]/BDMV/STREAM/00008.m2ts'
+path = r'BDMV/[BDMV][200429][Kimetsu no Yaiba][Vol.10]/BDMV/STREAM/00008.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
