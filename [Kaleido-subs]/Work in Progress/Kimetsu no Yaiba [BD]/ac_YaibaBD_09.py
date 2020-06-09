a0d9dfabdb8c118cb43ccec201345341ce246534
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-10 01:24:51 +0200

    [Kaleido] Update Yaiba Vol.4 (BD)

--- [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_09.py
+++ [Kaleido-subs]/Work in Progress/Kimetsu no Yaiba [BD]/ac_YaibaBD_09.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import acsuite as acs
 import lvsfunc as lvf
 ac = acs.AC()
@@ -8,4 +9,4 @@ path = r'BDMV/[BDMV][191030][Kimetsu no Yaiba][Vol.4]/BDMV/STREAM/00005.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, [(0, -24)], path[:-4]+"wav", "YaibaBD_09_cut.wav")
+    ac.eztrim(src, [(0, -24)], f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
