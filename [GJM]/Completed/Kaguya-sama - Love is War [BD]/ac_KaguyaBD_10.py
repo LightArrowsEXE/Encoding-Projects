4969b74c3e7dee259ae72cd90bd1a460487cae92
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-17 04:13:09 +0200

    [GJM] Update Kaguya (BD)

--- [GJM]/Completed/Kaguya-sama - Love is War [BD]/ac_KaguyaBD_10.py
+++ [GJM]/Completed/Kaguya-sama - Love is War [BD]/ac_KaguyaBD_10.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import os
-import acsuite as acs
+
 import lvsfunc as lvf
-ac = acs.AC()
+from acsuite import eztrim
 
 
 path = r'BDMV/かぐや様は告らせたい Vol.5/BD/BDMV/STREAM/00002.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, (0, -27), f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
+    eztrim(src, (0, -27), f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
