26bb401edd1dbb05fec8a4ff6c2974cd224dca35
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-17 05:28:22 +0200

    [Commie] Update Symphogear XV (BD) AC

--- Miscellaneous groups/[Commie]/Work in Progress/Symphogear XV [BD]/ac_SymphoXVBD_NCED1.py
+++ Miscellaneous groups/[Commie]/Work in Progress/Symphogear XV [BD]/ac_SymphoXVBD_NCED1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import acsuite
+import os
+from acsuite import eztrim
 import lvsfunc as lvf
-ac = acsuite.AC()
 
 # Note: NCs for this show have AC3 audio, not DTS-HD!
 path = r'BDMV/KIXA_90889/BDMV/STREAM/00008.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, [(24, -24)], path[:-4]+"flac", "SymphoXVBD_NCED1_cut.flac")
+    eztrim(src, (24, -24), f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
