ede7dc8e8ff8414d3a871370305e57965972e2a1
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-17 04:57:46 +0200

    [Chyuu] Upload Fate/EXTRA Last Encore (BD)

---
+++ Miscellaneous groups/[Chyuu]/Completed/Fate Extra Last Encore [BD]/ac_FELEBD_NCED1v03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+from acsuite import eztrim
+import lvsfunc as lvf
+
+
+path = r'BDMV/Fate Extra Last Encore/Vol1/BDROM/BDMV/STREAM/00014.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (24, -24), f"{os.path.splitext(path)[0]}.wav", f"{__file__[3:-3]}_cut.wav")
