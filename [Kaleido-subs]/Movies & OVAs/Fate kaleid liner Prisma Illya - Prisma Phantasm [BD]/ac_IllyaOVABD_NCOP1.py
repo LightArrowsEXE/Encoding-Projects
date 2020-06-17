172d8fb14c639c9046cfef82b14728ca14292cfd
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-06-17 05:40:29 +0200

    [Kaleido] Update Illya OVA (BD) AC

--- [Kaleido-subs]/Movies & OVAs/Fate kaleid liner Prisma Illya - Prisma Phantasm [BD]/ac_IllyaOVABD_NCOP1.py
+++ [Kaleido-subs]/Movies & OVAs/Fate kaleid liner Prisma Illya - Prisma Phantasm [BD]/ac_IllyaOVABD_NCOP1.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 import vapoursynth as vs
-import acsuite as acs
+from acsuite import eztrim
 import lvsfunc as lvf
 core = vs.core
-ac = acs.AC()
 
 
 path = r'BDMV/[BDMV][191127] Fate／kaleid liner Prisma☆Illya Prisma☆Phantasm/PRISMAPHANTASM_SP/BDMV/STREAM/00005.m2ts'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, [([24, -24])], path, "IllyaOVABD_NCOP1_cut.wav")
-
-src.set_output()
+    eztrim(src, (24, -24), f"{os.path.splitext(path)[0]}.wav", f"{__file__[:-3]}_cut.wav")
