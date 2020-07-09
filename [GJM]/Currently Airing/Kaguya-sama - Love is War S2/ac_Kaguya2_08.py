a4acced22e3253a550f9351d79fcefd9c4c460dc
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-07-09 04:46:16 +0200

    [GJM] Kaguya S2 01-08: Update AC files

--- [GJM]/Currently Airing/Kaguya-sama - Love is War S2/ac_Kaguya2_08.py
+++ [GJM]/Currently Airing/Kaguya-sama - Love is War S2/ac_Kaguya2_08.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import ntpath
 import os
-import acsuite as acs
+
 import lvsfunc as lvf
-ac = acs.AC()
+from acsuite import eztrim
 
 
 path = r'08/[HorribleSubs] Kaguya-sama wa Kokurasetai S2 - 08 [1080p].mkv'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    ac.eztrim(src, [(289, 0)], f"{os.path.splitext(path)[0]}_Track02.aac", f"{__file__[:-3]}_cut.aac")
+    eztrim(src, (289, 0), f"{os.path.splitext(path)[0]}.wav", f"{ntpath.basename(__file__)[3:-3]}_cut.wav")
