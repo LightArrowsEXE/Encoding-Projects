e2ad85b6a7aaacbff7bb06c8a88e561aacf92564
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-07-10 10:38:39 +0200

    [GJM] Kaguya S2 12: Upload script + AC

---
+++ [GJM]/Currently Airing/Kaguya-sama - Love is War S2/ac_Kaguya2_12.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import ntpath
+import os
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+
+path = r'12/[HorribleSubs] Kaguya-sama wa Kokurasetai S2 - 12 [1080p].mkv'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (289, 0), f"{os.path.splitext(path)[0]}.aac", f"{ntpath.basename(__file__)[3:-3]}_cut.aac")
