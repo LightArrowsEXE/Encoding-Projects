aa39fb23d93cb0b2f7dc20fa8aa838485b350d89
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-09-24 14:41:44 +0200

    [GJM] Kaguya S2: Update AC files
    Minor fix to make trimming easier.

--- [GJM]/Completed/Kaguya-sama - Love is War S2/ac_Kaguya2_03.py
+++ [GJM]/Completed/Kaguya-sama - Love is War S2/ac_Kaguya2_03.py
@@ -10,4 +10,4 @@ path = r'03/[HorribleSubs] Kaguya-sama wa Kokurasetai S2 - 03 [1080p].mkv'
 src = lvf.src(path)
 
 if __name__ == "__main__":
-    eztrim(src, (289, 0), f"{os.path.splitext(path)[0]}.aac", f"{ntpath.basename(__file__)[3:-3]}_cut.aac")
+    eztrim(src, (289, 0), f"{os.path.splitext(path)[0]}.mkv", f"{ntpath.basename(__file__)[3:-3]}_cut.aac", ffmpeg_path='')
