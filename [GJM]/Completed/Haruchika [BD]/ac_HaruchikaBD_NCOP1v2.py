1319b3788ed7876753db82e77fa769d0f922e40a
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-09-24 14:42:23 +0200

    [GJM] Haruchika: Upload files
    Annoying af to do, but whatevs

---
+++ [GJM]/Completed/Haruchika [BD]/ac_HaruchikaBD_NCOP1v2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import ntpath
+import os
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+
+path = r'ハルチカ ~ハルタとチカは青春する~/HARUCHIKA_2/BDMV/STREAM/00010.m2ts'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (24, -24), f"{os.path.splitext(path)[0]}.wav", f"{ntpath.basename(__file__)[3:-3]}_cut.wav")
