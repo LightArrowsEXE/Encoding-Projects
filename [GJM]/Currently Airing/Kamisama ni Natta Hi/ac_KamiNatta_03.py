f4da42c1c3c33e3fc39d60a7d0bbfe8e69fdfabe
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-10-25 15:15:57 +0100

    [GJM] Kamisama ni Natta hi: 03

---
+++ [GJM]/Currently Airing/Kamisama ni Natta Hi/ac_KamiNatta_03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import ntpath
+import os
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+
+ep = "03"  # The only annoying thing about using SubsPlease over Erai now is the CRC
+path = f'{ep}/[SubsPlease] Kamisama ni Natta Hi - {ep} (1080p) [08273E54].mkv'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (289, 0), path,
+           f"{ep}/{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')
