be2f1ebc28ca9857002b0c23feb2835ee7ec89c2
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-11-16 23:19:03 +0100

    [Kaleido] Majo no Tabitabi: 07

---
+++ [Kaleido-subs]/Currently Airing/Majo no Tabitabi/ac_Tabitabi_07.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import ntpath
+import os
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+
+ep = "07"  # The only annoying thing about using SubsPlease over Erai now is the CRC
+path = f'{ep}/[SubsPlease] Majo no Tabitabi - {ep} (1080p) [DD71CEC7].mkv'
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (240, 0), path,
+           f"{ep}/{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')
