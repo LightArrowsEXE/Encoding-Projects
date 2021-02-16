fd670bba893f08ad396a48c9fb0b3023487d15ea
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-02-16 08:32:17 +0100

    [Kaleido] Majo no Tabitabi (BD): Box 1

---
+++ [Kaleido-subs]/Work in Progress/Majo no Tabitabi [BD]/ac_TabitabiBD_03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ntpath
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+path = r"BDMV/[BDMV][アニメ][210127][MAJO_NO_TABITABI_1][Blu-Ray BOX 上]/BDMV/STREAM/00005.m2ts"
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (24, -24), path, f"{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')  # noqa
