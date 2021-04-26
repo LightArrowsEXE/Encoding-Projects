716cc555305f44bce293a11342ecdea3f8042115
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-04-26 03:40:39 +0200

    [Kaleido] The World Ends With You: 03

---
+++ [Kaleido-subs]/Currently Airing/The World Ends With You/ac_TWEWY_03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import glob
+import ntpath
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+
+path = glob.glob(f'{ntpath.basename(__file__)[-5:-3]}/*.mkv')[2]
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (168, 0), path, f"{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')  # noqa
