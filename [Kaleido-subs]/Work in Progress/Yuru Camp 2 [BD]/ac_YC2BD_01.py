c1b2d56e2de4ef1fd4cb2e8f7c26deea8bfa7cd5
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-04-26 05:39:34 +0200

    [Kaleido] Yuru Camp 2 (BD): Vol.1

---
+++ [Kaleido-subs]/Work in Progress/Yuru Camp 2 [BD]/ac_YC2BD_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ntpath
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+path = r"BDMV/[BDMV][210324][Yuru Camp Season 2][Vol.1]/BD/BDMV/STREAM/00004.m2ts"  # noqa
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (24, -48), path, f"{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')  # noqa
