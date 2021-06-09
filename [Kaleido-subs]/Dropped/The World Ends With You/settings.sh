69ee6304fb9dca30644a35490b7b89d783a3703d
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-06-09 07:52:02 +0200

    [Kaleido] The World Ends With You: Dropped

--- [Kaleido-subs]/Dropped/The World Ends With You/settings.sh
+++ [Kaleido-subs]/Dropped/The World Ends With You/settings.sh
@@ -10,9 +10,9 @@ for f in *.vpy; do
     --y4m -o "$(basename $f .vpy).265" - \
     --fps 24000/1001 --sar 1 --videoformat ntsc --range limited \
     --colormatrix bt709 --colorprim bt709 --transfer bt709 \
-    --preset veryslow --crf 15 --deblock=-2:-2 --keyint 240 --min-keyint 23  --ref 6 --bframes 16  \
+    --preset veryslow --crf 15 --deblock=-2:-2 --keyint 240 --min-keyint 23 --ref 6 --bframes 16 \
     --aq-mode 3 --aq-strength 0.85 --qcomp 0.70 --cbqpoffs -2 --crqpoffs -2 --rc-lookahead 60 \
-    --rd 3 --rdoq-level 2 --psy-rd 1.75 --psy-rdoq 2.0 --cutree \
+    --rd 3 --rdoq-level 2 --psy-rd 2.00 --psy-rdoq 2.0 --cutree \
     --no-open-gop --b-intra --weightb --tskip --no-strong-intra-smoothing --no-sao --no-sao-non-deblock \
     --output-depth 10 --qpfile "keyframes/$(basename "$f" .vpy)_keyframes.txt";
 
