6722d32e60e680c2cf798943d17d92d5ae62d61c
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-06-26 12:15:50 +0200

    [OPMan] Ao no Exorcist: OP1-2, ED1-2

---
+++ [OPMan]/Seasonals [TV]/2011-2 - Spring/[a8148] Ao no Exorcist/ac_ExorcistBD_NCED2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ntpath
+
+import lvsfunc as lvf
+from acsuite import eztrim
+
+path = r"E:/src/[BDMV][アニメ]青の祓魔師/[BDMV][アニメ][111123] BlueExorcist 6/BDROM/BDMV/STREAM/00006.m2ts"
+src = lvf.src(path)
+
+if __name__ == "__main__":
+    eztrim(src, (24, -25), path, f"{ntpath.basename(__file__)[3:-3]}_cut.mka", ffmpeg_path='')  # noqa
