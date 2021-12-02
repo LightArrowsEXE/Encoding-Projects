fbae64395b826fb6ce95c5bb802fffd531c0dbd3
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-12-03 00:26:39 +0100

    [OPMan] Idol Time PriPara (BD): NCs

---
+++ [OPMan]/Seasonals [TV]/2017-2 - Spring/[a12737] Idol Time PriPara/idoltimeBD_NCOP3v1.py
@@ -0,0 +1,119 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, Tuple, Union
+
+import vapoursynth as vs
+from lvsfunc.misc import source
+from vardautomation import FileInfo, PresetBD, PresetFLAC, VPath
+
+from project_module import encoder as enc
+from project_module import flt
+
+core = vs.core
+
+
+shader_file = 'assets/FSRCNNX_x2_16-0-4-1.glsl'
+if not Path(shader_file).exists():
+    hookpath = r"mpv/shaders/FSRCNNX_x2_16-0-4-1.glsl"
+    shader_file = os.path.join(str(os.getenv("APPDATA")), hookpath)
+
+
+# Sources
+JP_BD = FileInfo(r"E:/src/Idol Time PriPara/IDOL_TIME_PRIPARA_BDBOX4_D2/BDMV/STREAM/00016.m2ts", (24, -24),
+                 idx=lambda x: source(x, force_lsmas=True, cachedir=''), preset=[PresetBD, PresetFLAC])
+JP_BD.name_file_final = VPath(fr"premux/{JP_BD.name} (Premux).mkv")
+JP_BD.a_src_cut = JP_BD.name
+JP_BD.do_qpfile = True
+
+
+zones: Dict[Tuple[int, int], Dict[str, Any]] = {  # Zones for x265
+}
+
+
+def filterchain() -> Union[vs.VideoNode, Tuple[vs.VideoNode, ...]]:
+    """Main VapourSynth filterchain"""
+    import havsfunc as haf
+    import lvsfunc as lvf
+    import vardefunc as vdf
+    from ccd import ccd
+    from muvsfunc import SSIM_downsample
+    from vsutil import depth, get_y
+
+    src = JP_BD.clip_cut
+    src = depth(src, 32)
+
+    src_y = get_y(src)
+    l_mask = vdf.mask.FDOG().get_mask(src_y, lthr=0.065, hthr=0.065).std.Maximum().std.Minimum()
+    l_mask = l_mask.std.Median().std.Convolution([1] * 9)
+
+    # Rescaling
+    descale = flt.auto_descale(src_y)
+    desc_i = flt.descale_fields(src_y, kernel=lvf.kernels.BicubicSharp)
+    descale = lvf.rfs(descale, desc_i, [(1555, 1685)])
+
+    supersample = vdf.scale.nnedi3cl_double(descale, use_znedi=True, pscrn=1)
+    downscaled = SSIM_downsample(supersample, src.width, src.height, smooth=((3 ** 2 - 1) / 12) ** 0.5,
+                                 sigmoid=True, filter_param_a=0, filter_param_b=0)
+
+    scaled_mask = core.std.MaskedMerge(src_y, downscaled, l_mask)
+    scaled = depth(vdf.misc.merge_chroma(scaled_mask, src), 16)
+
+    # Denoising
+    l_mask_16 = depth(l_mask, 16).std.Minimum()
+    dft = core.dfttest.DFTTest(scaled, sigma=1.25, tbsize=3, tosize=1)
+    dft_masked = core.std.MaskedMerge(dft, scaled, l_mask_16)
+
+    ccd_uv = ccd(dft, threshold=4, matrix='709')
+    ccd_uv = core.std.MaskedMerge(ccd_uv, dft_masked, l_mask_16, planes=[1, 2])
+
+    decs = vdf.noise.decsiz(ccd_uv, sigmaS=4, min_in=212 << 8, max_in=240 << 8)
+
+    # AA and slight lineart enhancement
+    baa = lvf.aa.based_aa(decs, shader_file)
+    sraa = lvf.sraa(decs, rfactor=1.5, downscaler=lvf.kernels.Bicubic(b=-1/2, c=1/4).scale)
+    clmp = lvf.aa.clamp_aa(decs, baa, sraa, strength=1.5)
+
+    sraa_strong = flt.transpose_sraa(decs, rfactor=1.2, downscaler=lvf.kernels.Bicubic(b=-1/2, c=1/4).scale)
+    clmp = lvf.rfs(clmp, sraa_strong, [])
+
+    darken = haf.FastLineDarkenMOD(clmp, strength=36)
+
+    # Debanding and graining
+    deband = core.average.Mean([
+        flt.masked_f3kdb(darken, rad=18, thr=[28, 24]),
+        flt.masked_placebo(darken, rad=15, thr=4)
+    ])
+
+    grain = vdf.noise.Graigasm(
+        thrs=[x << 8 for x in (32, 80, 128, 176)],
+        strengths=[(0.15, 0.0), (0.10, 0.0), (0.10, 0.0), (0.0, 0.0)],
+        sizes=(1.15, 1.10, 1.05, 1),
+        sharps=(60, 50, 50, 50),
+        grainers=[
+            vdf.noise.AddGrain(seed=69420, constant=True),
+            vdf.noise.AddGrain(seed=69420, constant=True),
+            vdf.noise.AddGrain(seed=69420, constant=True)
+        ]).graining(deband)
+
+    return grain
+
+
+if __name__ == '__main__':
+    FILTERED = filterchain()
+    enc.Encoder(JP_BD, FILTERED).run(clean_up=True, zones=zones)  # type: ignore
+elif __name__ == '__vapoursynth__':
+    FILTERED = filterchain()
+    if not isinstance(FILTERED, vs.VideoNode):
+        raise ImportError(
+            f"Input clip has multiple output nodes ({len(FILTERED)})! Please output just 1 clip"
+        )
+    else:
+        enc.dither_down(FILTERED).set_output(0)
+else:
+    JP_BD.clip_cut.std.SetFrameProp('node', intval=0).set_output(0)
+    FILTERED = filterchain()
+    if not isinstance(FILTERED, vs.VideoNode):
+        for i, clip_filtered in enumerate(FILTERED, start=1):
+            clip_filtered.std.SetFrameProp('node', intval=i).set_output(i)
+    else:
+        FILTERED.std.SetFrameProp('node', intval=1).set_output(1)
---
+++ [OPMan]/Seasonals [TV]/2017-2 - Spring/[a12737] Idol Time PriPara/project_module/filter.py
@@ -0,0 +1,176 @@
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import vapoursynth as vs
+from lvsfunc.kernels import Catrom, Kernel
+from vsutil import depth, get_w, get_y, iterate, join, plane
+
+from .util import _get_bits
+
+core = vs.core
+
+
+def detail_mask(clip: vs.VideoNode,
+                sigma: float = 1.0, rxsigma: List[int] = [50, 200, 350],
+                pf_sigma: Optional[float] = 1.0, brz: Tuple[int, int] = (2500, 4500),
+                rg_mode: int = 17) -> vs.VideoNode:
+    """
+    A detail mask aimed at preserving as much detail as possible within darker areas,
+    even if it contains mostly noise.
+    """
+    bits, clip = _get_bits(clip)
+
+    clip_y = get_y(clip)
+    pf = core.bilateral.Gaussian(clip_y, sigma=pf_sigma) if pf_sigma else clip_y
+    ret = core.retinex.MSRCP(pf, sigma=rxsigma, upper_thr=0.005)
+
+    blur_ret = core.bilateral.Gaussian(ret, sigma=sigma)
+    blur_ret_diff = core.std.Expr([blur_ret, ret], "x y -")
+    blur_ret_dfl = core.std.Deflate(blur_ret_diff)
+    blur_ret_ifl = iterate(blur_ret_dfl, core.std.Inflate, 4)
+    blur_ret_brz = core.std.Binarize(blur_ret_ifl, brz[0])
+    blur_ret_brz = core.morpho.Close(blur_ret_brz, size=8)
+
+    prewitt_mask = core.std.Prewitt(clip_y).std.Binarize(brz[1])
+    prewitt_ifl = prewitt_mask.std.Deflate().std.Inflate()
+    prewitt_brz = core.std.Binarize(prewitt_ifl, brz[1])
+    prewitt_brz = core.morpho.Close(prewitt_brz, size=4)
+
+    merged = core.std.Expr([blur_ret_brz, prewitt_brz], "x y +")
+    rm_grain = core.rgvs.RemoveGrain(merged, rg_mode)
+    return rm_grain if bits == 16 else depth(rm_grain, bits)
+
+
+def placebo_debander(clip: vs.VideoNode, grain: int = 4, **deband_args: Any) -> vs.VideoNode:
+    return join([  # Still not sure why splitting it up into planes is faster, but hey!
+        core.placebo.Deband(plane(clip, 0), grain=grain, **deband_args),
+        core.placebo.Deband(plane(clip, 1), grain=0, **deband_args),
+        core.placebo.Deband(plane(clip, 2), grain=0, **deband_args)
+    ])
+
+
+def line_darkening(clip: vs.VideoNode, strength: float = 0.2, **kwargs: Any) -> vs.VideoNode:
+    """
+    Darken lineart through Toon.
+    Taken from varde's repository.
+    """
+    import havsfunc as haf
+
+    darken = haf.Toon(clip, strength, **kwargs)
+    darken_mask = core.std.Expr(
+        [core.std.Convolution(clip, [5, 10, 5, 0, 0, 0, -5, -10, -5], divisor=4, saturate=False),
+         core.std.Convolution(clip, [5, 0, -5, 10, 0, -10, 5, 0, -5], divisor=4, saturate=False)],
+        ['x y max {neutral} / 0.86 pow {peak} *'
+            .format(neutral=1 << (clip.format.bits_per_sample-1),  # type: ignore[union-attr]
+                    peak=(1 << clip.format.bits_per_sample)-1)])  # type: ignore[union-attr]
+    return core.std.MaskedMerge(clip, darken, darken_mask)
+
+
+def transpose_sraa(clip: vs.VideoNode, **kwargs: Any) -> vs.VideoNode:
+    from lvsfunc import sraa
+
+    aa = sraa(clip.std.Transpose(), **kwargs)
+    return sraa(aa.std.Transpose(), **kwargs)
+
+
+def masked_f3kdb(clip: vs.VideoNode,
+                 rad: int = 16,
+                 thr: Union[int, List[int]] = 24,
+                 grain: Union[int, List[int]] = [12, 0],
+                 mask_args: Dict[str, Any] = {}
+                 ) -> vs.VideoNode:
+    """Basic f3kdb debanding with detail mask"""
+    from debandshit import dumb3kdb
+
+    deb_mask_args: Dict[str, Any] = dict(brz=(1000, 2750))
+    deb_mask_args |= mask_args
+
+    bits, clip = _get_bits(clip)
+
+    deband_mask = detail_mask(clip, **deb_mask_args)
+
+    deband = dumb3kdb(clip, radius=rad, threshold=thr, grain=grain, seed=69420)
+    deband_masked = core.std.MaskedMerge(deband, clip, deband_mask)
+    deband_masked = deband_masked if bits == 16 else depth(deband_masked, bits)
+    return deband_masked
+
+
+def masked_placebo(clip: vs.VideoNode,
+                   rad: int = 12, thr: Union[int, List[int]] = 4,
+                   itr: int = 2, grain: int = 2,
+                   mask_args: Dict[str, Any] = {}
+                   ) -> vs.VideoNode:
+    """Basic placebo debanding with detail mask"""
+    deb_mask_args: Dict[str, Any] = dict(brz=(1750, 4000))
+    deb_mask_args |= mask_args
+
+    bits, clip = _get_bits(clip)
+
+    deband_mask = detail_mask(clip, **deb_mask_args)
+
+    deband = placebo_debander(clip, radius=rad, threshold=thr, grain=grain, iterations=itr)
+    deband_masked = core.std.MaskedMerge(deband, clip, deband_mask)
+    deband_masked = deband_masked if bits == 16 else depth(deband_masked, bits)
+    return deband_masked
+
+
+def auto_descale(clip: vs.VideoNode, height: int = 720, thr: float = 0.000015) -> vs.VideoNode:
+    """
+    Quick wrapper to descale to Sharp Bicubic and Spline36,
+    compare them, and then pick one or the other.
+
+    Could've sworn there was another function that did this, but I forgot what it was lol
+    """
+    from functools import partial
+
+    from lvsfunc.kernels import BicubicSharp, Spline36
+    from lvsfunc.util import get_prop
+
+    def _compare(n: int, f: vs.VideoFrame,
+                 sharp: vs.VideoNode,
+                 spline: vs.VideoNode) -> vs.VideoNode:
+        """Compare PlaneStatsDiff's"""
+        sharp_diff = get_prop(f[0], 'PlaneStatsDiff', float)
+        spline_diff = get_prop(f[1], 'PlaneStatsDiff', float)
+
+        return sharp if spline_diff - thr > sharp_diff else spline
+
+    clip_y = get_y(clip)
+    sharp = BicubicSharp().descale(clip_y, get_w(height, clip.width/clip.height), height)
+    sharp_up = BicubicSharp().scale(sharp, clip.width, clip.height)
+
+    spline = Spline36().descale(clip_y, get_w(height, clip.width/clip.height), height)
+    spline_up = Spline36().scale(spline, clip.width, clip.height)
+
+    # We need a diff between the rescaled clips and the original clip
+    sharp_diff = sharp_up.std.PlaneStats(clip_y)
+    spline_diff = spline_up.std.PlaneStats(clip_y)
+
+    # Extra props for future frame evalling in case it might prove useful (for credits, for example)
+    sharp = sharp.std.SetFrameProp('scaler', data='BicubicSharp')
+    spline = spline.std.SetFrameProp('scaler', data='Spline36')
+
+    return core.std.FrameEval(sharp, partial(_compare, sharp=sharp, spline=spline), [sharp_diff, spline_diff])
+
+
+def descale_fields(clip: vs.VideoNode, height: int = 720,
+                   tff: bool = True, kernel: Kernel = Catrom,
+                   src_top: float = 0.0) -> vs.VideoNode:
+    """
+    Simple descaling wrapper for interwoven upscaled fields
+
+    :param clip:        Input clip
+    :param height:      Native height. Will be divided by two internally.
+                        Width is autocalculated using height and the clip's aspect ratio.
+    :param tff:         Top-field-first
+    :param kernel:      lvsfunc.Kernel object
+
+    :return:            Descaled GRAY clip
+    """
+    height_field = height/2
+
+    clip = clip.std.SetFieldBased(2-int(tff))
+
+    sep = core.std.SeparateFields(get_y(clip))
+    descaled = kernel().descale(sep, get_w(height, clip.width/clip.height), height_field, [src_top, 0])
+    weave_y = core.std.DoubleWeave(descaled)[::2].std.SetFrameProp('scaler', data=f'{kernel.__name__} (Fields)')
+    return weave_y.std.SetFrameProp('_FieldBased', intval=0)
