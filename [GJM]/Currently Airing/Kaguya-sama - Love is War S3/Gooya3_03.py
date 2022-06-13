eab859b6ff1a375f3325f7e3749fb2e902cc5a56
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-13 02:24:22 +0200

    [GJM] Kaguya-sama S3: Update env

--- [GJM]/Currently Airing/Kaguya-sama - Love is War S3/Gooya3_03.py
+++ [GJM]/Currently Airing/Kaguya-sama - Love is War S3/Gooya3_03.py
@@ -1,38 +1,26 @@
-import multiprocessing as mp
-from typing import Any, Dict, List, Tuple, Union
+from __future__ import annotations
+
+import ntpath
+from glob import glob
+from typing import Any, Dict, List, Tuple
 
 import vapoursynth as vs
-from glob import glob
-import yaml
-import ntpath
-from lvsfunc.misc import source
-from lvsfunc.types import Range
-from vardautomation import FileInfo, PresetBD, VPath, get_vs_core
+import vsencode as vse
+from lvsfunc.types import VSDPIR_STRENGTH_TYPE, Range
 from vardefunc import initialise_input
 
-from project_module import encoder as enc
-from project_module import flt
+from project_module.filter import process_fileinfo
 
-with open("config.yaml", 'r') as conf:
-    config = yaml.load(conf, Loader=yaml.FullLoader)
+ini = vse.generate.init_project()
 
-core = get_vs_core(range(0, (mp.cpu_count() - 2)) if config['reserve_core'] else None)
+core = vse.util.get_vs_core(reserve_core=ini.reserve_core)
+
+shader = vse.get_shader("FSRCNNX_x2_56-16-4-1.glsl")
 
 
 # Sources
-JP_WEB = FileInfo(f"{config['bdmv_dir']}/{glob(f'{ntpath.basename(__file__)[-6:-4]}/*.mkv')[0]}.mkv",
-                  idx=lambda x: source(x, force_lsmas=True, cachedir=''), preset=[PresetBD])
-JP_NCED = None
-JP_WEB.name_file_final = VPath(fr"premux/{JP_WEB.name} (Premux).mkv")
-JP_WEB.a_src_cut = VPath(JP_WEB.name)
-
-
-# OP/ED scenefiltering
-opstart = False
-edstart = False
-op_offset = 1
-ed_offset = 1
-
+SRC = vse.FileInfo(f"{glob(f'{ini.bdmv_dir}/*{ntpath.basename(__file__)[-5:-3]} (*[*.mkv')[0]}")
+SRC = process_fileinfo(SRC)
 
 # Freezeframing
 ff_first: List[int] = [
@@ -44,8 +32,14 @@ ff_last: List[int] = [
 ff_repl: List[int] = [
 ]
 
-deblock_ranges: List[Range] = [
+
+# Scenefiltering
+no_scaled: List[Range] = [  # Ranges that should not be rescaled
 ]
+
+deblock_zones: List[Tuple[Range | List[Range], VSDPIR_STRENGTH_TYPE]] = [  # Ranges that require strong deblocking
+]
+
 
 zones: Dict[Tuple[int, int], Dict[str, Any]] = {  # Zones for the encoder
     (2218, 2331): {'b': 0.90},
@@ -53,105 +47,126 @@ zones: Dict[Tuple[int, int], Dict[str, Any]] = {  # Zones for the encoder
     (5493, 5537): {'b': 0.90},
 }
 
-
 for k, v in zones:
-    deblock_ranges += [(k, v)]
+    deblock_zones |= [((k, v), 50)]
 
-run_script: bool = __name__ == '__main__'
+deblock_ranges: List[Range] = []
+
+for zones in deblock_zones:
+    for range, _ in zones:
+        deblock_ranges |= range
+
+deblock_ranges |= [
+]
 
 
 @initialise_input(bits=32)
-def filterchain(src: vs.VideoNode = JP_WEB.clip_cut) -> Union[vs.VideoNode, Tuple[vs.VideoNode, ...]]:
-    """Main filterchain"""
+def filterchain(src: vs.VideoNode = SRC.clip_cut) -> vs.VideoNode | Tuple[vs.VideoNode, ...]:
+    """Main filterchain."""
+    import adptvgrnMod as adp
+    import debandshit as dbs
     import havsfunc as haf
     import lvsfunc as lvf
     import vardefunc as vdf
     import vsdenoise as vsd
-    from ccd import ccd
+    import vsmask as vsm
     from finedehalo import fine_dehalo
-    from vsmlrt import Backend
+    from vskernels import kernels
     from vsutil import depth, get_w, get_y, iterate
+
+    from project_module import flt
 
     assert src.format
 
-    src_y = get_y(src)
+    cdmgl = flt.chroma_demangle(src, return_sclip=True)
 
-    # Descaling and rescaling
-    l_mask = vdf.mask.FDOG().get_mask(src_y, lthr=0.125, hthr=0.027).rgsf.RemoveGrain(4).rgsf.RemoveGrain(4)
-    l_mask = l_mask.std.Minimum().std.Deflate().std.Median().std.Convolution([1] * 9)
-    sq_mask = lvf.mask.BoundingBox((4, 4), (src.width-4, src.height-4)).get_mask(src_y).std.Invert()
+    # Rescaling
+    with vdf.YUVPlanes(cdmgl) as planes:
+        src_y = planes.Y
 
-    # Probably incorrect, but the results seem to be good enough and credit mask catches very little.
-    # I don't see any obvious lancsoz ringing either. The fuck did they do to this?
-    descale = lvf.kernels.Catrom().descale(src_y, get_w(874), 874)
-    upscale = lvf.kernels.Catrom().scale(descale, src.width, src.height)
+        l_mask = vsm.edge.FDoG().edgemask(src_y, lthr=0.125, hthr=0.025).rgsf.RemoveGrain(4).rgsf.RemoveGrain(4)
+        l_mask = l_mask.std.Minimum().std.Deflate().std.Median().std.Convolution([1] * 9).std.Limiter()
+        sq_mask = lvf.mask.BoundingBox((4, 4), (src.width-4, src.height-4)).get_mask(src_y).std.Invert().std.Limiter()
 
-    credit_mask = lvf.scale.descale_detail_mask(src_y, upscale, threshold=0.075)
-    credit_mask = iterate(credit_mask, core.std.Inflate, 2)
-    credit_mask = iterate(credit_mask, core.std.Maximum, 2)
-    credit_mask = core.std.Expr([credit_mask, sq_mask], "x y -").std.Limiter()
+        descale = kernels.Catrom().descale(src_y, get_w(874), 874)
+        upscale = kernels.Catrom().scale(descale, src.width, src.height)
 
-    upscaled = vdf.scale.nnedi3_upscale(descale, use_znedi=True, pscrn=1)
-    downscale = lvf.scale.ssim_downsample(upscaled, src.width, src.height)
-    downscale = core.std.MaskedMerge(downscale, src_y, credit_mask)
-    scaled = depth(vdf.misc.merge_chroma(downscale, src), 16)
+        credit_mask = lvf.scale.descale_detail_mask(src_y, upscale, threshold=0.075)
+        credit_mask = iterate(credit_mask, core.std.Inflate, 2)
+        credit_mask = iterate(credit_mask, core.std.Maximum, 2)
+        credit_mask = core.akarin.Expr([credit_mask, sq_mask], "x y -").std.Limiter()
 
-    smd = haf.SMDegrain(scaled, tr=3, thSAD=50)
-    dft = smd.dfttest.DFTTest(sigma=4)
-    bm3d = vsd.BM3DCudaRTC(smd, ref=dft, sigma=[0.75, 0.0]).clip
-    den_uv = ccd(bm3d, threshold=7.5)
+        rescale = vdf.scale.fsrcnnx_upscale(descale, src.width, src.height, shader,
+                                            downscaler=lvf.scale.ssim_downsample,
+                                            overshoot=1.1, undershoot=1.5,
+                                            profile='slow', strength=25)
+        rescale_min = core.akarin.Expr([rescale, src_y], "x y min")
+        merge_lineart = core.std.MaskedMerge(src_y, depth(rescale_min, 32), l_mask)
+        merge_credits = core.std.MaskedMerge(merge_lineart, src_y, credit_mask)
+        planes.Y = lvf.rfs(merge_credits, src_y, no_scaled)
 
-    debl = lvf.deblock.vsdpir(den_uv, strength=75, tiles=8,
-                              backend=Backend.TRT(fp16=True) if run_script
-                              else Backend.ORT_CUDA(fp16=True))
-    darken = haf.FastLineDarkenMOD(debl, strength=20)
-    debl = lvf.rfs(den_uv, darken, deblock_ranges)
+    scaled = depth(planes.clip, 16)
 
-    decs = vdf.noise.decsiz(debl, min_in=200 << 8, max_in=240 << 8)
+    # Denoising
+    ref = haf.SMDegrain(get_y(scaled), tr=3, thSAD=150, Str=2.5)
+    bm3d = depth(vsd.BM3DCudaRTC(scaled, [0.55, 0], radius=3, ref=ref).clip, 32)
+    wnnm = core.wnnm.WNNM(bm3d, [0, 3.0], radius=0, group_size=7, bm_range=9)
+    decs = vdf.noise.decsiz(depth(wnnm, 16), min_in=200 << 8, max_in=240 << 8)
 
-    dering = haf.HQDeringmod(decs, mthr=40, nrmode=2, sharp=0, darkthr=0)
-    dehaloa = haf.FineDehalo(decs, rx=1.85, ry=1.85, thma=164, darkstr=0)
-    dehalo = core.std.Expr([decs, dering, dehaloa], "x y - abs x z - abs < y z ?")
-    dehalo = fine_dehalo(decs, dehalo)
+    # Sometimes a scene is so heavily blocked, we need to deblock it.
+    debl = lvf.vsdpir(decs, strength=0, tiles=8, overlap=8, zones=deblock_zones)
+    debl = lvf.rfs(decs, debl, deblock_ranges)
+
+    dering = haf.HQDeringmod(debl, mthr=24, nrmode=2, sharp=0, darkthr=0)
+    fdehalo = haf.FineDehalo(debl, rx=2, ry=2, thma=204, darkstr=0)
+    dehalo = core.std.Expr([debl, dering, fdehalo], "x y - abs x z - abs < y z ?")
+    dehalo = fine_dehalo(debl, dehalo)
+
+    ec = haf.EdgeCleaner(dehalo, strength=15)
+    ec = core.akarin.Expr([dehalo, ec], "y 32768 < x y min x y max ?")
 
     if ff_first:
-        dehalo = dehalo.std.FreezeFrames(ff_first, ff_last, ff_repl)
+        ec = ec.std.FreezeFrames(ff_first, ff_last, ff_repl)
 
-    detail_mask = lvf.mask.detail_mask(dehalo, rad=4, brz_a=0.015, brz_b=0.05)
+    detail_mask = lvf.mask.detail_mask(ec, rad=4, brz_a=0.015, brz_b=0.05)
     deband = core.std.MaskedMerge(core.average.Mean([
-        flt.masked_f3kdb(dehalo, rad=17, thr=24, grain=[12, 6]),
-        flt.masked_f3kdb(dehalo, rad=21, thr=[32, 24], grain=[24, 12]),
-        flt.masked_placebo(dehalo, rad=10, thr=3.5, grain=4)
-    ]), dehalo, detail_mask)
+        dbs.dumb3kdb(ec, radius=18, threshold=[20, 16], grain=[12, 6]),
+        dbs.dumb3kdb(ec, radius=21, threshold=[32, 24], grain=[24, 12]),
+        dbs.placebo_deband(ec, threshold=3, iterations=2, grain=[4, 0])
+    ]), ec, detail_mask)
 
-    grain = vdf.noise.Graigasm(
-        thrs=[x << 8 for x in (32, 80, 128, 176)],
-        strengths=[(0.125, 0.0), (0.10, 0.0), (0.05, 0.0), (0.0, 0.0)],
-        sizes=(1.0, 1.0, 1.0, 1.0),
-        sharps=(80, 65, 50, 50),
-        grainers=[
-            vdf.noise.AddGrain(seed=69420, constant=True),
-            vdf.noise.AddGrain(seed=69420, constant=True),
-            vdf.noise.AddGrain(seed=69420, constant=True)
-        ]).graining(deband)
+    grain = adp.adptvgrnMod(deband, luma_scaling=10, static=False, temporal_average=50,
+                            grainer=lambda x: core.noise.Add(x, xsize=3.5, ysize=3.5, var=2.0, uvar=0.35,
+                                                             type=3, every=2, seed=69420))
 
     return grain
 
 
+FILTERED = filterchain()
+
+
 if __name__ == '__main__':
-    FILTERED = filterchain()
-    enc.Encoder(JP_WEB, FILTERED).run(zones=zones)  # type: ignore
+    assert isinstance(FILTERED, vs.VideoNode)
+    vse.EncodeRunner(SRC, FILTERED) \
+        .video('x265', '.settings/x265_settings', zones=zones) \
+        .audio('aac') \
+        .mux('LightArrowsEXE@GoodJobMedia').run()
 elif __name__ == '__vapoursynth__':
-    FILTERED = filterchain()
     if not isinstance(FILTERED, vs.VideoNode):
-        raise ImportError(f"Input clip has multiple output nodes ({len(FILTERED)})! Please output just 1 clip")
+        raise vs.Error(f"Input clip has multiple output nodes ({len(FILTERED)})! Please output a single clip")
     else:
-        enc.dither_down(FILTERED).set_output(0)
+        vse.video.finalize_clip(FILTERED).set_output(0)
 else:
-    JP_WEB.clip_cut.std.SetFrameProp('node', intval=0).set_output(0)
-    FILTERED = filterchain()
+    SRC.clip_cut.set_output(0)
+
     if not isinstance(FILTERED, vs.VideoNode):
         for i, clip_filtered in enumerate(FILTERED, start=1):
-            clip_filtered.std.SetFrameProp('node', intval=i).set_output(i)
+            clip_filtered.set_output(i)
+            if i == 10:
+                print("Warning! More than 10 output nodes set! Stopping more from being output...")
+                break
     else:
-        FILTERED.std.SetFrameProp('node', intval=1).set_output(1)
+        FILTERED.set_output(1)
+
+    for i, audio_node in enumerate(SRC.audios_cut, start=10):
+        audio_node.set_output(i)
